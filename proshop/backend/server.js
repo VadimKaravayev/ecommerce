cfa8be3f636ce346f8016fdcf6a2d5f8b9ba70be
vadymkaravayev <vadym_karavayev_tmg@whirlpool.com>
2024-07-06 13:43:52 +0300

    started backend

---
+++ proshop/backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const id = req.params.id;
+  const product = products.find((p) => p._id === id);
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port: ${port}`);
+});
--- proshop/frontend/src/screens/ProductScreen.jsx
+++ proshop/frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function ProductScreen() {
   const { id } = useParams();
-  const product = products.find(p => p._id === id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+      console.log(data)
+    }
+    fetchProduct();
+  }, [id]);
+
 
   return <>
     <Link className="btn btn-light my-3" to="/" >Go back</Link>
